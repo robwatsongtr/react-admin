21c6fad3583863e1649b58ad03d4d3b4dc9914c0
robwatsongtr <rwatso@gmail.com>
2022-06-05 08:28:19 -0700

    adding good comments

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ import jsonServerProvider from 'ra-data-json-server';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 // The App component renders an <Admin> component, which is the root component
-// of a react-admin app. This component expect a dataProvider prop.
+// of a react-admin app. 
+
+// This component expect a dataProvider prop.
 // The admin component expects one or more <Resource> child components.
 // Each resource maps a NAME to an ENDPOINT. 
+
+// The line <Resource name="users" /> informs react-admin 
+// to fetch the “users” records from the 
+// https://jsonplaceholder.typicode.com/users URL. 
+// <Resource> also defines the React components to use for each CRUD operation 
+// (list (Read), create, edit(update, delete), and show).
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
