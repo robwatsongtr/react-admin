6464bd048e60ae340ffd9a2dcd3117f4a657a8c7
robwatsongtr <rwatso@gmail.com>
2022-06-07 15:17:20 -0700

    added inline styles to MyUrlField.js

--- src/MyUrlField.js
+++ src/MyUrlField.js
@@ -1,15 +1,22 @@
 import * as React from "react";
 import { useRecordContext } from 'react-admin';
+import { Link } from '@mui/material';
+import LaunchIcon from '@mui/icons-material/Launch';
+
 
 // in react-admin, fields are simple React components. At runtime
 // they grab the the record fetched from the API with a custom hook
 
+// React relies on MUI, which support a prop called 'sx', which allows
+// custom inline styles. 
+
 const MyUrlField = ({ source }) => {
     const record = useRecordContext();
     return record ? (
-        <a href={record[source]}>
+        <Link href={record[source]} sx={{ textDecoration: 'none'}}>
             {record[source]}
-        </a>
+            <LaunchIcon sx={{ width: '0.5em', height: '0.5em', paddingLeft: 1 }} />
+        </Link>
     ) : null;
 }
 
